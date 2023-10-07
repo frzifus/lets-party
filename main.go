5775b1ae1795da46eefa61123622508610fe0e5c
Benedikt Bongartz <bongartz@klimlive.de>
2023-10-07 15:17:00 +0200

    traceing: configure otlp gRPC exporter
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log/slog"
+	"os"
+	"time"
 
 	"net/http"
 
 	"github.com/google/uuid"
+	"go.opentelemetry.io/otel"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/frzifus/lets-party/intern/db/jsondb"
 	"github.com/frzifus/lets-party/intern/model"
@@ -15,10 +23,38 @@ import (
 )
 
 func main() {
+
 	var (
 		serviceName = *flag.String("service-name", "party-invite", "otel service name")
 		addr        = *flag.String("addr", "0.0.0.0:8080", "default server address")
+		otlpAddr = *flag.String("otlp-grpc", "localhost:4317", "default otlp/gRPC address")
 	)
+	textHandler := slog.NewTextHandler(os.Stdout, nil)
+	logger := slog.New(textHandler)
+
+	logger.Info("start and listen", "address", addr)
+	logger.Info("otlp/gRPC", "address", otlpAddr, "service", serviceName)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	grpcOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock()}
+	conn, err := grpc.DialContext(ctx, otlpAddr, grpcOptions...)
+	if err != nil {
+		logger.Error("failed to create gRPC connection to collector", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	// Set up a trace exporter
+	otelExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		logger.Error("failed to create trace exporter", err)
+		os.Exit(1)
+	}
+	tp := sdktrace.NewTracerProvider(sdktrace.WithSyncer(otelExporter))
+
+	otel.SetTracerProvider(tp)
 
 	guestsStore, _ := jsondb.NewGuestStore("testdata/guests.json")
 
@@ -42,6 +78,7 @@ func main() {
 		fmt.Printf("invitation(%d): %s with guests: %s", i, invite.ID, invite.GuestIDs)
 	}
 
+	logger.Info("stats", "invitations", len(invitations), "guests", len(guests))
 	srv := &http.Server{
 		Addr: addr,
 		Handler: server.NewServer(
