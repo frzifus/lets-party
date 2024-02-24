77c5fd24f78769695423106af2ec0173716e2082
Ben B <bongartz@klimlive.de>
2024-02-24 18:12:08 +0100

    event: parse event edits (#54)
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>
    Co-authored-by: Moritz Fleck <24444046+schogges@users.noreply.github.com>

--- intern/model/event.go
+++ intern/model/event.go
@@ -7,27 +7,24 @@ import (
 )
 
 type Event struct {
-	ID        uuid.UUID   `json:"id"`
-	CreatedAt *time.Time  `json:"created_at"`
-	UpdatedAt *time.Time  `json:"updated_at"`
-	Location  *Location   `json:"location"`
-	Date      time.Time   `json:"date"`
-	Hotels    []*Location `json:"hotels,omitempty"`
-	Airports  []*Location `json:"airports,omitempty"`
+	*Location
+	Date     time.Time   `json:"date" form:"date"`
+	Hotels   []*Location `json:"hotels,omitempty" form:"hotels"`
+	Airports []*Location `json:"airports,omitempty" form:"airports"`
 }
 
 type Location struct {
-	ID           uuid.UUID  `json:"id"`
-	CreatedAt    *time.Time `json:"created_at"`
-	UpdatedAt    *time.Time `json:"omitempty,updated_at"`
-	Name         string     `json:"name,omitempty"`
-	URL          string     `json:"url,omitempty"`
-	Country      string     `json:"country,omitempty"`
-	City         string     `json:"city,omitempty"`
-	ZipCode      string     `json:"zipcode,omitempty"`
-	Street       string     `json:"street,omitempty"`
-	StreetNumber string     `json:"street_number,omitempty"`
-	Longitude    float64    `json:"longitude,omitempty"`
-	Latitude     float64    `json:"latitude,omitempty"`
-	Website      string     `json:"website,omitempty"`
+	ID           uuid.UUID  `json:"id" form:"-"`
+	CreatedAt    *time.Time `json:"created_at" form:"-"`
+	UpdatedAt    *time.Time `json:"updated_at,omitempty" form:"-"`
+	Name         string     `json:"name,omitempty" form:"name"`
+	URL          string     `json:"url,omitempty" form:"url"`
+	Country      string     `json:"country,omitempty" form:"country"`
+	City         string     `json:"city,omitempty" form:"city"`
+	ZipCode      string     `json:"zipcode,omitempty" form:"zipcode"`
+	Street       string     `json:"street,omitempty" form:"street"`
+	StreetNumber string     `json:"street_number,omitempty" form:"street_number"`
+	Longitude    float64    `json:"longitude,omitempty" form:"longitude"`
+	Latitude     float64    `json:"latitude,omitempty" form:"latitude"`
+	Website      string     `json:"website,omitempty" form:"website"`
 }
