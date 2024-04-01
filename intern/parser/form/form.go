bd804cd3751c66c8dbbac29932805732cca89838
Benedikt Bongartz <bongartz@klimlive.de>
2024-04-01 18:48:28 +0200

    parser/form: support float64 and recursive ptrs
    Signed-off-by: Benedikt Bongartz <bongartz@klimlive.de>

--- intern/parser/form/form.go
+++ intern/parser/form/form.go
@@ -12,8 +12,7 @@ import (
 // in the value pointed to by target. If target is nil or not a pointer,
 // Unmarshal returns an [InvalidUnmarshalError].
 //
-// Unmarshal is currently still limited. Given list values will be ignored and
-// the only supported slice type is string.
+// Unmarshal is currently still to a few primitive types and basic recursion.
 func Unmarshal(input url.Values, target any) error {
 	val := reflect.ValueOf(target)
 	if val.Kind() != reflect.Ptr || val.IsNil() {
@@ -52,11 +51,33 @@ func Unmarshal(input url.Values, target any) error {
 				return err
 			}
 			fieldVal.SetInt(int64(intValue))
+		case reflect.Float64:
+			if fieldValRaw == "" {
+				continue
+			}
+			fValue, err := strconv.ParseFloat(fieldValRaw, 64)
+			if err != nil {
+				return err
+			}
+			fieldVal.SetFloat(fValue)
 		case reflect.Slice:
-			// NOTE: We assume the slice is of type string.
-			// if not... we panic... because we can.
-			// TODO: Create reflect.Slice and call Unmarshal.
-			fieldVal.Set(reflect.AppendSlice(fieldVal, reflect.ValueOf(value)))
+			sliceValue := reflect.ValueOf(value)
+			for i := 0; i < sliceValue.Len(); i++ {
+				if isPrimitiveType(sliceValue.Type().Elem().Kind()) {
+					fieldVal.Set(reflect.Append(fieldVal, sliceValue.Index(i)))
+					continue
+				}
+
+				var ptr any
+				if sliceValue.Kind() == reflect.Ptr {
+					ptr = sliceValue.Index(i).Interface()
+				} else {
+					ptr = sliceValue.Index(i).Addr().Interface()
+				}
+				if err := Unmarshal(input, ptr); err != nil {
+					return err
+				}
+			}
 		case reflect.Struct:
 			newInput := make(url.Values, len(input))
 			for k, v := range input {
@@ -71,6 +92,17 @@ func Unmarshal(input url.Values, target any) error {
 		}
 	}
 	return nil
+}
+
+func isPrimitiveType(kind reflect.Kind) bool {
+	switch kind {
+	case reflect.Bool, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64, reflect.String:
+		return true
+	default:
+		return false
+	}
 }
 
 type InvalidUnmarshalError struct {
