f5ccad855153c66ce7f6e990b571cb091c407951
Janik <10290002+led0nk@users.noreply.github.com>
2024-05-18 21:37:00 +0200

    added portal with temporary templates
    portal init release

---
+++ internal/portal/handler.go
@@ -0,0 +1,33 @@
+package portal
+
+import "net/http"
+
+func (p *Portal) home(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	err := p.templates.TmplHome.Execute(w, nil)
+	if err != nil {
+		p.logger.ErrorContext(ctx, "failed to execute template", "error", err)
+		return
+	}
+}
+
+func (p *Portal) login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	err := p.templates.TmplLogin.Execute(w, nil)
+	if err != nil {
+		p.logger.ErrorContext(ctx, "failed to execute template", "error", err)
+		return
+	}
+}
+
+func (p *Portal) register(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	err := p.templates.TmplRegister.Execute(w, nil)
+	if err != nil {
+		p.logger.ErrorContext(ctx, "failed to execute template", "error", err)
+		return
+	}
+}
---
+++ internal/portal/tmp/templatehandler.go
@@ -0,0 +1,28 @@
+package templates
+
+import (
+	"embed"
+	"text/template"
+)
+
+type TemplateHandler struct {
+	TmplHome     *template.Template
+	TmplLogin    *template.Template
+	TmplRegister *template.Template
+}
+
+//go:embed templates/*.html
+var templates embed.FS
+
+func NewTemplateHandler() *TemplateHandler {
+	mainTemplate := []string{"templates/main.html", "templates/main.style.html", "templates/header.html", "templates/nav.html", "templates/footer.html"}
+	homeTemplate := "templates/home.html"
+	loginTemplate := "templates/login.html"
+	registerTemplate := "templates/register.html"
+
+	return &TemplateHandler{
+		TmplHome:     template.Must(template.ParseFS(templates, append(mainTemplate, homeTemplate)...)),
+		TmplLogin:    template.Must(template.ParseFS(templates, append(mainTemplate, loginTemplate)...)),
+		TmplRegister: template.Must(template.ParseFS(templates, append(mainTemplate, registerTemplate)...)),
+	}
+}
